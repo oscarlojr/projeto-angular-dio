36f43754db70d817c9b3301c1f998c541f952b6f
Oscar Junior <oscarjunior@MacBook-Pro-de-Oscar.local>
2022-07-10 20:05:21 -0300

    Criando tabelas dentro do componente

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,9 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
 
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'Angular: Forms',
+        imageUrl: '',
+        price: 99.99,
+        code: 'XPS-8796',
+        duration: 120,
+        rating: 4.4,
+        releaseDate: 'December, 2, 2019',
+      },
+      {
+        id: 2,
+        name: 'Angular: HTTP',
+        imageUrl: '',
+        price: 45.99,
+        code: 'LPL-1073',
+        duration: 90,
+        rating: 5.0,
+        releaseDate: 'December, 21, 2020',
+      },
+    ]
+  }
 }
