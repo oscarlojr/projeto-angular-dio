1459f29d75bdc6d3088a09ed5042ef2903f6da74
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2022-07-24 19:54:25 -0300

    Criando nav bar e página de erro 404 com rotas

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,42 +1,6 @@
-// import { Component, OnInit } from "@angular/core";
-// import { Course } from "./course";
-
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
 import { CourseService } from "./course.service";
-
-// @Component({
-//   selector: 'app-course-list',
-//   templateUrl: './course-list.component.html'
-// })
-// export class CourseListComponent implements OnInit {
-//   courses: Course[] = [];
-
-//   ngOnInit(): void {
-//     this.courses = [
-//       {
-//         id: 1,
-//         name: 'Angular: Forms',
-//         imageUrl: '',
-//         price: 99.99,
-//         code: 'XPS-8796',
-//         duration: 120,
-//         rating: 4.4,
-//         releaseDate: 'December, 2, 2019',
-//       },
-//       {
-//         id: 2,
-//         name: 'Angular: HTTP',
-//         imageUrl: '',
-//         price: 45.99,
-//         code: 'LPL-1073',
-//         duration: 90,
-//         rating: 5.0,
-//         releaseDate: 'December, 21, 2020',
-//       },
-//     ]
-//   }
-// }
 
 @Component({
   selector: 'app-course-list',
