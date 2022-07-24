50d9c28d899c8a84c18eee3bc14486ec829a98c6
Oscar Junior <oscarjunior@MacBook-Pro-de-Oscar.local>
2022-07-24 10:48:22 -0300

    Criando filtro de cursos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -43,13 +43,25 @@ import { CourseService } from "./course.service";
   templateUrl: './course-list.component.html',
 })
 export class CourseListComponent implements OnInit {
-  courses: Course[] = [];
+
+  filteredCourses: Course[] = [];
+  _courses: Course[] = [];
+  _filterBy!: string;
 
   constructor(private CourseService: CourseService){
     
   }
 
   ngOnInit(): void{
-    this.courses = this.CourseService.retrieveAll();
+    this._courses = this.CourseService.retrieveAll();
+    this.filteredCourses = this._courses;
+  }
+
+  set filter(value: string){
+    this._filterBy = value;
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+  }
+  get filter() {
+    return this._filterBy;
   }
 }
